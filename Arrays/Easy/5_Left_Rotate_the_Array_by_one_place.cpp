80c2cbb4fd041e06c4af89572dfa27333d0c8162
Faizan_08 <faizanahmadsiddiqui026@gmail.com>
2026-06-04 13:34:34 +0530

    Add files via upload

---
+++ Arrays/Easy/5_Left_Rotate_the_Array_by_one_place.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr={1,1,2,2,2,3,3};
+    int n=arr.size();
+    int temp=arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
